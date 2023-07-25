12564a2ba8782319b259043f9d3f9bc3fbb297dc
Filipe Serafini <filipe.farah@edu.pucrs.br>
2023-07-25 15:11:02 -0300

    criacao projeto xcode

---
+++ BookApp/BookApp/BookAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  BookAppApp.swift
+//  BookApp
+//
+//  Created by Filipe Serafini on 25/07/23.
+//
+
+import SwiftUI
+
+@main
+struct BookAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
