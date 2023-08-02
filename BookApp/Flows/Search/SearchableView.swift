a6227a53f1ba07bdc4d16053834159d8b6423065
Waldyr Schneider <Waldyr.Schneider@edu.pucrs.br>
2023-08-02 13:56:55 -0300

    feature: paginação para os livros procurados

--- BookApp/Flows/Search/SearchableView.swift
+++ BookApp/Flows/Search/SearchableView.swift
@@ -1,73 +1,72 @@
 import SwiftUI
 
 struct SearchableView: View {
+    private let spaceName = "scroll"
+    @State private var scrollViewSize: CGSize = .zero
+    @State private var wholeSize: CGSize = .zero
     @State private var searchText = ""
     @State private var filteredData: [String] = []
     @EnvironmentObject private var searchViewModel: SearchViewModel
     
-    let spaceName = "scroll"
-    @State var scrollViewSize: CGSize = .zero
-    
     var body: some View {
-        ScrollView {
-            ChildSizeReader(size: $scrollViewSize) {
-                VStack(alignment: .leading) {
-                    // Search Bar
-                    HStack {
-                        TextField("Procure por livros, autores e gêneros", text: $searchText)
-                            .onSubmit {
-                                searchViewModel.fetchBooks(searchedText: searchText)
-                            }
-                            .padding(15)
-                            .font(.system(size: 15))
-                            .foregroundColor(Color(red: 0.255, green: 0.255, blue: 0.255))
-                    }
-                    .padding(.trailing, 20)
-                    .background(Color(red: 0.851, green: 0.851, blue: 0.851))
-                    .cornerRadius(30)
-                    .padding()
-                    .overlay(
+        ChildSizeReader(size: $wholeSize) {
+            ScrollView {
+                ChildSizeReader(size: $scrollViewSize) {
+                    VStack(alignment: .leading) {
+                        // Search Bar
                         HStack {
-                            Spacer()
-                            Button(action: {
-                                searchViewModel.fetchBooks(searchedText: searchText)
-                                
-                                print("Pesquisar...")
-                            }) {
-                                Image("searchIconSelected")
-                                    .padding(.trailing, 25)
+                            TextField("Procure por livros, autores e gêneros", text: $searchText)
+                                .onSubmit {
+                                    searchViewModel.fetchBooks(searchedText: searchText)
+                                }
+                                .padding(15)
+                                .font(.system(size: 15))
+                                .foregroundColor(Color(red: 0.255, green: 0.255, blue: 0.255))
+                        }
+                        .padding(.trailing, 20)
+                        .background(Color(red: 0.851, green: 0.851, blue: 0.851))
+                        .cornerRadius(30)
+                        .padding()
+                        .overlay(
+                            HStack {
+                                Spacer()
+                                Button(action: {
+                                    searchViewModel.fetchBooks(searchedText: searchText)
+                                }) {
+                                    Image("searchIconSelected")
+                                        .padding(.trailing, 25)
+                                }
+                                .disabled(searchText == "")
+                                .opacity(searchText == "" ? 0.6 : 1)
                             }
-                            .disabled(searchText == "")
-                            .opacity(searchText == "" ? 0.6 : 1)
+                        )
+                        
+                        ForEach(searchViewModel.books) { book in
+                            ResearchedBookView(book: book)
+                        }
+                    }
+                    .background(
+                        GeometryReader { proxy in
+                            Color.clear.preference(
+                                key: ViewOffsetKey.self,
+                                value: -1 * proxy.frame(in: .named(spaceName)).origin.y
+                            )
                         }
                     )
-                    
-                    ForEach(searchViewModel.books) { book in
-                        ResearchedBookView(book: book)
-                    }
+                    .onPreferenceChange(
+                        ViewOffsetKey.self,
+                        perform: { value in
+                            if value > scrollViewSize.height - wholeSize.height {
+                                if !searchText.isEmpty {
+                                    searchViewModel.fetchBooks(searchedText: searchText)
+                                }
+                            }
+                        }
+                    )
                 }
-                .background(
-                    GeometryReader { proxy in
-                        Color.clear.preference(
-                            key: ViewOffsetKey.self,
-                            value: -1 * proxy.frame(in: .named(spaceName)).origin.y
-                        )
-                    }
-                )
-                .onPreferenceChange(
-                    ViewOffsetKey.self,
-                    perform: { value in
-                        print("offset: \(value.description)") // offset: 1270.3333333333333 when User has reached the bottom
-                        print("height: \(scrollViewSize.height)") // height: 2033.3333333333333
-                        
-                        //                        if value > 900 {
-                        //                            searchViewModel.fetchBooks(searchedText: searchText)
-                        //                        }
-                    }
-                )
             }
+            .coordinateSpace(name: spaceName)
         }
-        .coordinateSpace(name: spaceName)
         .onChange(
             of: scrollViewSize,
             perform: { value in
@@ -120,42 +119,3 @@ struct Searchable_Previews: PreviewProvider {
         SearchableView()
     }
 }
-
-//import SwiftUI
-//
-//struct ItemListView: View {
-//    @StateObject private var viewModel = ItemViewModel()
-//
-//    var body: some View {
-//        ScrollViewReader { scrollViewProxy in
-//            ScrollView {
-//                LazyVStack {
-//                    ForEach(viewModel.items) { item in
-//                        ItemRow(item: item)
-//                            .onAppear {
-//                                viewModel.loadMoreItems(currentItem: item)
-//                            }
-//                    }
-//                }
-//                .padding()
-//            }
-//            .onAppear {
-//                viewModel.fetchItems()
-//            }
-//            .onChange(of: viewModel.items) { newItems in
-//                withAnimation {
-//                    scrollViewProxy.scrollTo(newItems.last?.id)
-//                }
-//            }
-//        }
-//    }
-//}
-//
-//struct ItemRow: View {
-//    let item: Item
-//
-//    var body: some View {
-//        // Implemente a aparência de uma linha de item individual aqui
-//        // Por exemplo: Text(item.name)
-//    }
-//}
