bcef1ce6a813b583dc006a5a52c5f0f6d2a25a93
Waldyr Schneider <Waldyr.Schneider@edu.pucrs.br>
2023-08-15 14:43:13 -0300

    salvando nota e mostrando no perfil

--- BookApp/Flows/Profile/MediumCommentButton.swift
+++ BookApp/Flows/Profile/MediumCommentButton.swift
@@ -1,15 +1,33 @@
 import SwiftUI
 
 struct MediumCommentButton: View {
+    
+    var comment: CommentReview
+    
     var body: some View {
-        RoundedRectangle(cornerRadius: 8)
-            .fill(Color("commentButton"))
-            .frame(width: 175, height: 100)
+        
+        HStack {
+            ZStack {
+                RoundedRectangle(cornerRadius: 8)
+                    .fill(Color("commentButton"))
+                VStack(alignment: .leading, spacing: 4) {
+                    Text(comment.bookTitle)
+                        .fontWeight(.semibold)
+                        .foregroundColor(Color("comment"))
+                        .frame(width: 165, height: 20, alignment: .leading)
+                    Text(comment.comment)
+                        .font(.subheadline)
+                }
+                .padding(.all, 4)
+                .frame(width: 165, height: 95, alignment: .topLeading)
+            }
+            .frame(width: 175, height: 105)
+        }
     }
 }
 
 struct MediumCommentButton_Previews: PreviewProvider {
     static var previews: some View {
-        MediumCommentButton()
+        MediumCommentButton(comment: CommentReview(comment: "Não gostei", bookID: "123", bookTitle: "Velozes e Furiosos")!)
     }
 }
--- BookApp/Flows/Profile/ProfileViewModel.swift
+++ BookApp/Flows/Profile/ProfileViewModel.swift
@@ -1,1 +1,33 @@
 import Foundation
+import CloudKit
+import SwiftUI
+
+class ProfileViewModel: ObservableObject {
+    
+    @Published var allCommentReviews: [CommentReview] = []
+    
+    func fetchCommentReview() {
+        CloudKitUtility.fetchUserRecordID { (result: Result<CKRecord.ID, Error>) in
+            switch result {
+            case .success(let recordID):
+                print("aa", recordID)
+            case .failure(let failure):
+                print(failure.localizedDescription)
+            }
+        }
+    }
+    
+    func addCommentReview(comment: CommentReview) {
+        print("adicionando comentario: ", comment.bookTitle)
+        allCommentReviews.append(comment)
+        
+//        CloudKitUtility.add(item: comment) { result in
+//            switch result {
+//            case .success(_):
+//                break
+//            case .failure(let error):
+//                print(error.localizedDescription)
+//            }
+//        }
+    }
+}
