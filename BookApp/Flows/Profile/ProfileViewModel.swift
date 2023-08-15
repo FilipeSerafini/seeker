6781c5a06089fef28d7090f5ec157cf8868dd733
Waldyr Schneider <Waldyr.Schneider@edu.pucrs.br>
2023-08-15 16:24:11 -0300

    feature: fazendo integracao com o banco e puxando notas do usuario

--- BookApp/Flows/Profile/ProfileViewModel.swift
+++ BookApp/Flows/Profile/ProfileViewModel.swift
@@ -7,12 +7,38 @@ class ProfileViewModel: ObservableObject {
     @Published var allCommentReviews: [CommentReview] = []
     
     func fetchCommentReview() {
+        print("pegando reviews do usuario...")
         CloudKitUtility.fetchUserRecordID { (result: Result<CKRecord.ID, Error>) in
             switch result {
             case .success(let recordID):
-                print("aa", recordID)
+                let reference = CKRecord.Reference(recordID: recordID, action: .none)
+                let predicate = NSPredicate(format: "creatorUserRecordID == %@", reference)
+                let recordType = "CommentReview"
+                
+                CloudKitUtility.fetch(predicate: predicate, recordType: recordType) { (result: Result<[CommentReview], Error>) in
+                    switch result {
+                    case .success(let userCommentReviews):
+                        print("sucesso em pegar as reviews do usuario")
+                        DispatchQueue.main.async {
+                            self.verifyNewComment(newComments: userCommentReviews)
+//                            self.allCommentReviews = userCommentReviews
+                        }
+                    case .failure(let failure):
+                        print("erro ao pegar reviews do banco")
+                        print(failure.localizedDescription)
+                    }
+                }
+                
             case .failure(let failure):
                 print(failure.localizedDescription)
+            }
+        }
+    }
+    
+    private func verifyNewComment(newComments: [CommentReview]) {
+        for newComment in newComments {
+            if !allCommentReviews.contains(where: { $0.id == newComment.id }) {
+                allCommentReviews.append(newComment)
             }
         }
     }
@@ -21,13 +47,14 @@ class ProfileViewModel: ObservableObject {
         print("adicionando comentario: ", comment.bookTitle)
         allCommentReviews.append(comment)
         
-//        CloudKitUtility.add(item: comment) { result in
-//            switch result {
-//            case .success(_):
-//                break
-//            case .failure(let error):
-//                print(error.localizedDescription)
-//            }
-//        }
+        CloudKitUtility.add(item: comment) { result in
+            switch result {
+            case .success(_):
+                print("salvo no cloud kit com sucesso!")
+                break
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
     }
 }
